2442aeb3a45fcb3805fb06e6e4be45773b4736bc
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2024-01-11 11:41:55 -0300

    primer

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+* sql con peliculas(nombre,director,categoria,resumen,estudio,actores,calificacion,presupuesto,recaudacion),
+* persona(informacion personal, trabajo),
+* estudio(nombre,),
+* calificacion(tomatazo,...)
+* conexion con sql
+* introducir datos al sql
+* modificar datos al sql
+* ventanas
+* buscador
+* peliculas mejor valoradas*/
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
